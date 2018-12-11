35e1d3eee189e74110e0144980847f4ef2da25e0
5lineofcode <5lineofcode@gmail.com>
2018-12-11 07:39:53 +0700

    second commit - versi berantakan
    second commit - versi berantakan

--- lib/linkedin/home.dart
+++ lib/linkedin/home.dart
@@ -77,56 +77,287 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: <Widget>[
                     Container(
                       height: 50.0,
-                      color: Colors.green,
-                    ),
-                    Container(
-                      height: 200.0,
-                      color: Colors.black,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           Container(
-                            width: deviceSize.width - 100,
-                            color: Colors.red,
+                            padding: EdgeInsets.all(10.0),
+                            child: Text("Deny, siapkan akun Anda"),
                           ),
-                          Container(
-                            width: deviceSize.width - 100,
-                            color: Colors.blue,
+                          Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Icon(Icons.close),
                           ),
                         ],
                       ),
                     ),
                     Container(
-                      height: 50.0,
-                      margin: EdgeInsets.only(bottom: 10.0),
-                      color: Colors.brown,
+                      height: 200.0,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          Container(
+                            width: deviceSize.width - 100,
+                            padding: EdgeInsets.all(4.0),
+                            child: Card(
+                              color: Colors.grey,
+                              child: Container(
+                                color: Colors.white,
+                                child: Column(
+                                  children: <Widget>[
+                                    Row(
+                                      children: <Widget>[
+                                        Image.asset("res/images/icon/news.png"),
+                                        Expanded(
+                                          child: Text(
+                                              "Dapatkan berita dan wawasan di feed Anda!"),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                        "Dapatkan update dari pakar industri. Mulai dengan mengikuti 5 orang yang disukai."),
+                                    Text(
+                                      "Ikuti Profesional Terkemuka",
+                                      style: TextStyle(
+                                        color: Color(0xFF0091CA),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: deviceSize.width - 30,
+                            padding: EdgeInsets.all(4.0),
+                            child: Card(
+                              color: Colors.grey,
+                              child: Container(
+                                color: Colors.white,
+                                child: Column(
+                                  children: <Widget>[
+                                    Text("1"),
+                                    Text(
+                                        "Langkah lagi untuk menegaskan profil Anda"),
+                                    Row(
+                                      children: <Widget>[
+                                        Column(
+                                          children: <Widget>[
+                                            Container(
+                                              color: Color(0xFF469a1f),
+                                              height: 10.0,
+                                              width: 80.0,
+                                            ),
+                                            Text(
+                                              "Upload Photo",
+                                              style: TextStyle(fontSize: 12.0),
+                                            ),
+                                          ],
+                                        ),
+                                        Container(
+                                          width: 10.0,
+                                        ),
+                                        Column(
+                                          children: <Widget>[
+                                            Container(
+                                              color: Color(0xFF469a1f),
+                                              height: 10.0,
+                                              width: 80.0,
+                                            ),
+                                            Text(
+                                              "Profile",
+                                              style: TextStyle(fontSize: 12.0),
+                                            ),
+                                          ],
+                                        ),
+                                        Container(
+                                          width: 10.0,
+                                        ),
+                                        Column(
+                                          children: <Widget>[
+                                            Container(
+                                              color: Color(0xFF469a1f),
+                                              height: 10.0,
+                                              width: 80.0,
+                                            ),
+                                            Text(
+                                              "Profile",
+                                              style: TextStyle(fontSize: 12.0),
+                                            ),
+                                          ],
+                                        ),
+                                        Container(
+                                          width: 10.0,
+                                        ),
+                                        Column(
+                                          children: <Widget>[
+                                            Container(
+                                              color: Color(0xFFe1e9ee),
+                                              height: 10.0,
+                                              width: 80.0,
+                                            ),
+                                            Text(
+                                              "Ikuti",
+                                              style: TextStyle(fontSize: 12.0),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
               Container(
-                height: 50.0,
+                height: 60.0,
+                padding: EdgeInsets.only(left: 25.0, right: 25.0),
                 margin: EdgeInsets.only(bottom: 10.0),
-                color: Colors.red,
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage("res/images/photo/deny.jpg"),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        "Bagikan pendapat atau foto Anda",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 17.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Container(
-                height: 250.0,
                 margin: EdgeInsets.only(bottom: 10.0),
                 color: Colors.red,
                 child: Column(
                   children: <Widget>[
                     Container(
                       height: 50.0,
-                      color: Colors.blue,
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                "B. J. Habibie",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(" mengomentari ini"),
+                            ],
+                          ),
+                          Icon(
+                            FontAwesomeIcons.ellipsisV,
+                            size: 14.0,
+                          ),
+                        ],
+                      ),
                     ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.black,
+                    Container(
+                      color: Colors.white,
+                      padding: EdgeInsets.all(10.0),
+                      width: deviceSize.width,
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("res/images/photo/faisal.jpg"),
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    "Faisal Ramdan Mulyadi",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14.0,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Flutter Programmer (Android & iOS)",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12.0,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Provinsi Jawa Barat",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12.0,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Text(
+                              "Your time is limited, so don't waste it living someone else's life. - Steve Jobs"),
+                        ],
                       ),
                     ),
                     Container(
                       height: 50.0,
-                      color: Colors.green,
+                      color: Colors.white,
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(FontAwesomeIcons.thumbsUp),
+                                Container(
+                                  padding: EdgeInsets.only(left: 5.0),
+                                  child: Text("Suka"),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.chat),
+                                Container(
+                                  padding: EdgeInsets.only(left: 5.0),
+                                  child: Text("Komentar"),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(FontAwesomeIcons.share),
+                                Container(
+                                  padding: EdgeInsets.only(left: 5.0),
+                                  child: Text("Bagikan"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -134,6 +365,21 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: Text('Profile'))
+        ],
       ),
     );
   }
